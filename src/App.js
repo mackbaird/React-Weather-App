b12dd39c90cbb7638d2167de20147f0714193282
Mackenzie <mackbaird@outlook.com>
2024-06-30 22:27:35 +0100

    Added Netlify and GitHub Links
    -Added links to GItHub repository and Netlify permalink.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,22 @@ export default function App() {
         >
           Mackenzie Baird
         </a>
-        , is open-sourced on GitHub, and hosted on Netlify
+        , is open-sourced on{" "}
+        <a
+          href="https://github.com/mackbaird/React-Weather-App"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+        , and hosted on{" "}
+        <a
+          href="https://6681ccc23d9dce9c63cba710--baird-react-weather.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
       </footer>
     </div>
   );
